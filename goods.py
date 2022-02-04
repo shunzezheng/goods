371f328e765166b727968da1013f1be750306ebe
zhengshunze <zhengshunze2020@gmail.com>
2022-02-04 23:17:04 +0800

    OK

--- goods.py
+++ goods.py
@@ -1,13 +1,13 @@
 # '''
-# 目前版本 v1.1.5
+# 目前版本 v1.2.0
 # 撰寫者:zeze
 # '''
-import re
 
-import MySQLdb
-
+# 若無安裝套件則選是否要自動安裝
 try:
     import os
+    import re
+    import MySQLdb
     from urllib.request import urlopen
     # import MySQLdb
     import requests
@@ -15,46 +15,43 @@ try:
 except ModuleNotFoundError:
     Promote = input("錯誤: 尚未安所需的套件! 是否自動安裝所需套件(Y/n)? : ")
     if Promote == "Y":
-        command_1 = 'pip install BeautifulSoup4'
-        command_2 = 'pip install requests'
-        command_3 = 'pip install urllib3'
-        command_4 = 'pip install lxml'
-        os.system(command_1)
-        os.system(command_2)
-        os.system(command_3)
-        os.system(command_4)
+        command = 'pip install BeautifulSoup4 requests urllib3 lxml mysqlclient'
+        os.system(command)
         basename = os.path.basename(__file__)
-        os.system('python ' + basename)
+        os.system('python ' + basename)  # 執行此命令
         quit()
     elif Promote == 'n':
         exit()
 
-list = []
+# 建立db連線到本地端資訊
+db = MySQLdb.connect(host="localhost",
+                     user="root",
+                     passwd="password",
+                     db="carrefour")
 
 
+# db連線
 def connection():
-    db = MySQLdb.connect(host="localhost",
-                         user="root",
-                         passwd="password",
-                         db="carrefour")
     cursor = db.cursor()
     cursor.execute('SELECT * FROM carrefour.goods')
     global results
     results = cursor.fetchall()
 
 
-def rgood():
-    global content, text
+# 爬蟲獲取商品名稱、價格、熱銷、縮網址
+def goods():
+    global content, text, list
     text = input("請輸入欲查詢商品的關鍵字: ")
     content = ""
+    list = []
     url = 'https://online.carrefour.com.tw/zh/search?q=' + str(text)
-    headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"}
+    headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                             "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"}
     response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, "lxml")
+    soup = BeautifulSoup(response.text, "lxml")  # Parser選用lxml，較為快速(?!)
     # num = int(input("您想要查看前幾名熱銷商品: "))
-    v = soup.find_all('a', class_='gtm-product-alink', limit=5)
-
+    v = soup.find_all('a', class_='gtm-product-alink', limit=5)  # 前五名
+    # 撈資料
     for s in v:
         global category
         serach = s.get('href')
@@ -66,7 +63,9 @@ def rgood():
         content += f"\n{category}\n{name}\t{price}\n{link}\n"
 
 
+# 查找商品的字詞與db的category產生關聯
 def find():
+    global n_area
     try:
         category = [record[0] for record in results]
         area = [record[3] for record in results]
@@ -80,10 +79,15 @@ def find():
                 n_area = re.sub(r"\[|\]|\'", "", str(l_area)).replace(',', '、')
         print(text + '可能在: ' + n_area + ' 走道區域')
         print("以下是商品前五名熱銷結果:\n" + content)
-    except:
-        print('錯誤:資料庫未建立種類資訊!', "\n以下是商品前五名熱銷結果:\n" + content)
+    except NameError:
+        if len(list) == 0:
+            print("商品不存在!")
+        print('錯誤:資料庫未建立種類資訊!', "\n以下是商品有關連性的結果(若無結果，請檢查是否輸入有誤!):" + content)
+    else:
+        print("以下是商品前五名熱銷結果:\n" + content)
 
 
+# 縮網址
 def shorten(long_url, alias):
     URL = "http://tinyurl.com/create.php?source=indexpage&url=" + long_url + "&submit=Make+TinyURL%21&alias=" + alias
     response = urlopen(URL)
@@ -92,15 +96,10 @@ def shorten(long_url, alias):
 
 
 if __name__ == "__main__":
-    while 1 == 1:
-        connection()
-        rgood()
-        find()
-        if len(list) == 0:
-            print("商品不存在!")
-        else:
-            Next = input("是否要繼續搜尋? (y/n) : ")
-            if Next == 'y':
-                rgood()
-            elif Next == 'n':
-                quit()
+    try:
+        while 1 == 1:
+            connection()
+            goods()
+            find()
+    except:
+        db.close()
